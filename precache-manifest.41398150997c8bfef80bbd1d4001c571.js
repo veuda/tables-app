ca9c0b053cdfbb2d7d864c3a7986c871c9a64138
veuda <62720180+veuda@users.noreply.github.com>
2021-05-14 22:50:41 +0200

    gh-pages

---
+++ precache-manifest.41398150997c8bfef80bbd1d4001c571.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e588be7d69adf11dce4c",
+    "url": "/tables-app/css/app.33348959.css"
+  },
+  {
+    "revision": "810d2f8d3883e3a7acad",
+    "url": "/tables-app/css/exercice.0a5dd127.css"
+  },
+  {
+    "revision": "ba1bcd805b583b14f01f",
+    "url": "/tables-app/css/table.579862ae.css"
+  },
+  {
+    "revision": "7347aadc32a317e34c24aedc1243ad11",
+    "url": "/tables-app/index.html"
+  },
+  {
+    "revision": "e588be7d69adf11dce4c",
+    "url": "/tables-app/js/app.b085a7a4.js"
+  },
+  {
+    "revision": "7e04882c6b7a26826d06",
+    "url": "/tables-app/js/chunk-vendors.4a72472b.js"
+  },
+  {
+    "revision": "810d2f8d3883e3a7acad",
+    "url": "/tables-app/js/exercice.1dc6a989.js"
+  },
+  {
+    "revision": "ba1bcd805b583b14f01f",
+    "url": "/tables-app/js/table.6ed84e02.js"
+  },
+  {
+    "revision": "be68ac5a68937e1f74a05d791a2d0ceb",
+    "url": "/tables-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tables-app/robots.txt"
+  }
+]);
